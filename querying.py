150fd212445ca9de5e566b2ff9374a4349900d7a
Liber-R <romolo.66@hotmail.it>
2025-03-20 17:43:18 +0100

    script base per fare query a graphdb

---
+++ querying.py
@@ -0,0 +1,24 @@
+import requests
+
+# Define the server URL and repository ID
+server_url = "http://localhost:7200" # url per collegamenti esterni: http://DESKTOP-2TOL1V5:7200/repositories/SemDigLib
+repo_id = "SemDigLib"
+
+# Define the SPARQL query
+query = """SELECT ?s ?p ?o WHERE { ?s ?p ?o . }"""
+
+# Set the headers
+headers = {
+    'Accept': 'application/sparql-results+json',
+    'Content-Type': 'application/sparql-query'
+}
+
+# Send the query to the repository
+response = requests.post(f"{server_url}/repositories/{repo_id}/query", data=query, headers=headers)
+
+# Check if the operation was successful
+if response.status_code == 200:
+    print("Query executed successfully.")
+    print(response.json())
+else:
+    print("Failed to execute query.")
